bb0c00048e7fb9ab662300b24d33f65002524307
joshuacassidy <Joshcassidy79@gmail.com>
2017-11-05 14:30:44 +0000

    Added methods for Bubble Sort using recursion

--- BubbleSort.java
+++ BubbleSort.java
@@ -15,7 +15,31 @@ public class BubbleSort <T extends Comparable<T>> {
 
     public void showArray(T[] listOfItems){
         for (T i: listOfItems){
-            System.out.print(i + " ");
+            System.out.printf("%d ",i);
         }
     }
+
+    public T[] recursiveSort(T[] listOfItems,int i){
+        if(i < listOfItems.length-1){
+            listOfItems = recursiveSortComparison(listOfItems,i,0);
+            i++;
+            return recursiveSort(listOfItems,i);
+        }
+        return listOfItems;
+    }
+
+    public T[] recursiveSortComparison(T[] listOfItems,int i,int j){
+        if(j < listOfItems.length-i-1){
+            if(listOfItems[j].compareTo(listOfItems[j+1]) > 0){
+                T temp = listOfItems[j+1];
+                listOfItems[j+1] = listOfItems[j];
+                listOfItems[j] = temp;
+            }
+            j++;
+            return recursiveSortComparison(listOfItems,i,j);
+        }
+        return listOfItems;
+
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BubbleSort<Integer> bubbleSort = new BubbleSort<>();
-        bubbleSort.showArray(bubbleSort.sort(new Integer[] {323,52,4,12,536,6}));
+        bubbleSort.showArray(bubbleSort.recursiveSort(new Integer[] {-1,34,35,534,-30,46,54,546323,52,4,12,536,6},0));
 
     }
 
